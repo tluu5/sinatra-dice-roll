a7099e283625c25ff4b1a0d794cea4d99cc4e51e
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2023-12-27 05:44:51 +0000

    commit working dice roll game

--- dice.rb
+++ dice.rb
@@ -8,46 +8,56 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+get("/") do 
+  erb(:homepage)
+end
+
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
+  2.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @rolls = []
+  1.times do
+    die = rand(1..20)
+    @rolls.push(die)
+  end
 
-  outcome = "You rolled a #{first_die}."
-
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  @rolls = []
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
 
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, and #{fourth_die} for a total of #{sum}."
+  erb(:five_four)
+end
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+get("/dice/100/6") do
+  @rolls = []
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+  
+  erb(:one_hundred_six)
 end
